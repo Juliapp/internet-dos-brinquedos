1ed2ace289387e6bfdf6cfcbb449bcc79ed18f70
Juli Aragão <aragaopintojuli@gmail.com>
2019-06-18 18:11:26 -0300

    Classe JSONConversor inserida

---
+++ Servidor/src/util/JSONConversor.java
@@ -0,0 +1,21 @@
+package util;
+
+import comunicacao.Mensagem;
+
+
+//import org.
+
+public abstract class JSONConversor {
+    
+    
+    public Mensagem JSONToMensagem(){
+        return null;
+        //return new Mensagem();
+        
+    }
+    
+    public void MensagemToJSON(){
+        
+    }
+    
+}
